999c92eaa0fede8a73913d2b78ddc93704687708
Charlie McVicker <charlie@mcvickernet.com>
2022-08-03 15:37:01 -0400

    refresh data; clean up data more

---
+++ src/data/clean-cll-data.ts
@@ -0,0 +1,58 @@
+import jsonDeck from "./cll1-v3-with-audio-file.json";
+import jsonTermsByLesson from "./terms_by_lesson.json";
+
+type JsonCard = typeof jsonDeck["cards"][number];
+
+export interface Card {
+  cherokee: string;
+  syllabary: string;
+  alternate_pronounciations: string[];
+  alternate_syllabary: string[];
+  english: string;
+  cherokee_audio: string[];
+  english_audio: string[];
+}
+
+export const cards = jsonDeck.cards
+  .filter((card) => card.data.challenge_files.length)
+  .map(cleanCard);
+
+export function cherokeeToKey(cherokee: string) {
+  return cherokee
+    .trim()
+    .toLowerCase()
+    .replaceAll(/[\.\?\,]/g, "");
+}
+
+export function keyForCard(card: Card): string {
+  return cherokeeToKey(card.cherokee);
+}
+
+function cleanCard(card: JsonCard): Card {
+  return {
+    cherokee: cherokeeToKey(card.data.challenge),
+    syllabary: card.data.syllabary.split(";")[0].trim(),
+    english: card.data.answer.replace(/^“([^“”]*)”\.$/, "$1"),
+    cherokee_audio: card.data.challenge_files.map(
+      ({ file }) => `${process.env.PUBLIC_URL}/source/chr/${file}`
+    ),
+    english_audio: card.data.answer_files.map(
+      ({ file }) => `${process.env.PUBLIC_URL}/source/en/${file}`
+    ),
+    alternate_pronounciations: [
+      ...card.data.challenge.split(";").slice(1).map(cherokeeToKey),
+    ],
+    alternate_syllabary: [
+      ...card.data.syllabary.split(";").map((s) => s.trim()),
+    ],
+  };
+}
+
+console.log("Available cards", cards.length);
+
+export const termsByLesson = Object.fromEntries(
+  Object.entries(jsonTermsByLesson).map(([lessonName, terms]) => [
+    lessonName,
+    terms.map((term) => cherokeeToKey(term.split(";")[0])),
+  ])
+);
