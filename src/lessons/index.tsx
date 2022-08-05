3f0f1e0adfde22436dc96442a535847df7390c72
Charlie McVicker <charlie@mcvickernet.com>
2022-08-05 12:14:54 -0400

    disable lesson links when terms have been practiced today

--- src/lessons/index.tsx
+++ src/lessons/index.tsx
@@ -2,6 +2,9 @@ import React, { ReactElement } from "react";
 import styled from "styled-components";
 import { StyledLink } from "../components/StyledLink";
 import { termsByLesson } from "../data/clean-cll-data";
+import { getTerms } from "../data/utils";
+import { useLeitnerBoxContext } from "../utils/LeitnerBoxProvider";
+import { LeitnerBoxState, TermStats } from "../utils/useLeitnerBoxes";
 
 const StyledLessonList = styled.ul`
   padding: 0;
@@ -26,7 +29,32 @@ const StyledLessonLinks = styled.div`
   gap: 8px;
 `;
 
+function termNeedsPractice(
+  term: TermStats | undefined,
+  epochNow: number
+): boolean {
+  if (!term) return true;
+  else return term.nextShowTime < epochNow + 1000 * 60 * 60;
+}
+
+/**
+ * Count how many terms from the lesson need to be practiced in the next hour
+ */
+function termsToPractice(
+  terms: string[],
+  leitnerBoxes: LeitnerBoxState,
+  epochNow: number
+) {
+  return terms.reduce(
+    (count, term) =>
+      termNeedsPractice(leitnerBoxes.terms[term], epochNow) ? count + 1 : count,
+    0
+  );
+}
+
 export function Lessons(): ReactElement {
+  const { state: leitnerBoxState } = useLeitnerBoxContext();
+  const now = Date.now();
   return (
     <div>
       <p>
@@ -37,20 +65,44 @@ export function Lessons(): ReactElement {
         You may choose to review only new terms in a chapter, or all terms
         introduced before and within that chapter.
       </p>
+      <p>
+        If a link to practice is disabled, you have already practiced those
+        terms enough for today.
+      </p>
       <StyledLessonList>
-        {Object.keys(termsByLesson).map((chapters, i) => (
-          <li key={i}>
-            <h2>{chapters}</h2>
-            <StyledLessonLinks>
-              <StyledLink to={`/practice/lesson/${chapters}/false`}>
-                New terms
-              </StyledLink>
-              <StyledLink to={`/practice/lesson/${chapters}/true`}>
-                All material
-              </StyledLink>
-            </StyledLessonLinks>
-          </li>
-        ))}
+        {Object.keys(termsByLesson).map((chapters, i) => {
+          return (
+            <li key={i}>
+              <h2>{chapters}</h2>
+              <StyledLessonLinks>
+                <StyledLink
+                  to={`/practice/lesson/${chapters}/false`}
+                  disabled={
+                    termsToPractice(
+                      getTerms(chapters, false),
+                      leitnerBoxState,
+                      now
+                    ) === 0
+                  }
+                >
+                  New terms
+                </StyledLink>
+                <StyledLink
+                  to={`/practice/lesson/${chapters}/true`}
+                  disabled={
+                    termsToPractice(
+                      getTerms(chapters, true),
+                      leitnerBoxState,
+                      now
+                    ) === 0
+                  }
+                >
+                  All material
+                </StyledLink>
+              </StyledLessonLinks>
+            </li>
+          );
+        })}
       </StyledLessonList>
     </div>
   );
