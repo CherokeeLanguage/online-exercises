688dcf9397a9a5f2fb5dc9bd5dc34f9a3e7d8440
Charlie McVicker <charlie@mcvickernet.com>
2022-08-10 14:33:03 -0400

    clean up browse lessons

--- src/views/lessons/BrowseLessons.tsx
+++ src/views/lessons/BrowseLessons.tsx
@@ -1,6 +1,9 @@
 import React, { ReactElement } from "react";
 import { StyledAnchor, StyledLink } from "../../components/StyledLink";
-import { useLessons } from "../../spaced-repetition/LessonsProvider";
+import {
+  nameForLesson,
+  useLessons,
+} from "../../spaced-repetition/LessonsProvider";
 
 export function BrowseLessons(): ReactElement {
   const { lessons, todaysLessons } = useLessons();
@@ -11,15 +14,17 @@ export function BrowseLessons(): ReactElement {
         <h3>Today's lessons</h3>
         <div>
           {todaysLessons.length ? (
-            todaysLessons.map((l, i) => (
-              <p key={i}>
-                {l.id} -- {l.terms.length} terms
-                <StyledLink to={`/practice/${l.id}`}>Continue</StyledLink>
-              </p>
-            ))
+            todaysLessons
+              .filter((lesson) => lesson.completedAt === null)
+              .map((l, i) => (
+                <p key={i}>
+                  {nameForLesson(l)} - {l.terms.length} terms
+                  <StyledLink to={`/practice/${l.id}`}>Continue</StyledLink>
+                </p>
+              ))
           ) : (
             <p>
-              No lessons today. Think about adding a new vocab set to your
+              No more lessons today. Think about adding a new vocab set to your
               rotation!
             </p>
           )}
