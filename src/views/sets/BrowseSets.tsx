868d33a2205ac100c1b34c3432d5f8f993f383c0
Charlie McVicker <charlie@mcvickernet.com>
2022-08-10 14:30:34 -0400

    clean up set browsing

--- src/views/sets/BrowseSets.tsx
+++ src/views/sets/BrowseSets.tsx
@@ -1,29 +1,20 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement } from "react";
 import styled from "styled-components";
 import { StyledLink } from "../../components/StyledLink";
-import { TermCardList } from "../../components/TermCardList";
-import {
-  Card,
-  cards,
-  keyForCard,
-  termsByLesson,
-} from "../../data/clean-cll-data";
-import { getTerms } from "../../data/utils";
 import { useLeitnerBoxContext } from "../../spaced-repetition/LeitnerBoxProvider";
-import { useCardsForTerms } from "../../utils/useCardsForTerms";
-import { TermStats } from "../../spaced-repetition/types";
 import { LeitnerBoxState } from "../../spaced-repetition/useLeitnerBoxes";
 import { getToday } from "../../utils/dateUtils";
 import { termNeedsPractice } from "../../spaced-repetition/groupTermsIntoLessons";
+import { collections, Set } from "../../data/sets";
 
-const StyledLessonList = styled.ul`
+const StyledSetList = styled.ul`
   padding: 0;
   margin: auto;
   max-width: 800px;
   list-style: none;
 `;
 
-const StyledLessonLinks = styled.div`
+const StyledSetLinks = styled.div`
   gap: 8px;
 `;
 
@@ -46,10 +37,6 @@ export function BrowseSets(): ReactElement {
   return (
     <div>
       <p>
-        Here you can select lessons from the Cherokee Language Lessons book to
-        review. These same terms are used in the audio exercises from the book.
-      </p>
-      <p>
         You may choose to review only new terms in a chapter, or all terms
         introduced before and within that chapter.
       </p>
@@ -57,11 +44,17 @@ export function BrowseSets(): ReactElement {
         If a link to practice is disabled, you have already practiced those
         terms enough for today.
       </p>
-      <StyledLessonList>
-        {Object.keys(termsByLesson).map((lessonName, i) => {
-          return <Lesson key={i} lessonName={lessonName} />;
-        })}
-      </StyledLessonList>
+      {Object.values(collections).map((collection) => (
+        <div>
+          <h2>{collection.title}</h2>
+
+          <StyledSetList>
+            {collection.sets.map((set, i) => {
+              return <VocabSet key={i} set={set} />;
+            })}
+          </StyledSetList>
+        </div>
+      ))}
     </div>
   );
 }
@@ -79,54 +72,24 @@ const StyledLessonHeader = styled.div`
   }
 `;
 
-export function Lesson({ lessonName }: { lessonName: string }): ReactElement {
+export function VocabSet({ set }: { set: Set }): ReactElement {
   const { state: leitnerBoxState } = useLeitnerBoxContext();
-  const [showTerms, setShowTerms] = useState(false);
   const today = getToday();
-  const lessonTerms = getTerms(lessonName, false);
-  const termCards = useCardsForTerms(cards, lessonTerms, keyForCard);
-
-  const termsWithStats = Object.entries(termCards).map<{
-    term: string;
-    stats?: TermStats;
-    card: Card;
-  }>(([term, card]) => ({
-    term,
-    stats: leitnerBoxState.terms[term] ?? undefined,
-    card,
-  }));
 
   return (
     <li>
       <StyledLessonHeader>
-        <h2>{lessonName}</h2>
-        <StyledLessonLinks>
+        <h2>{set.title}</h2>
+        <StyledSetLinks>
+          <StyledLink to={`/sets/${set.id}`}>View terms</StyledLink>
           <StyledLink
-            to={`/practice/lesson/${lessonName}/false`}
-            disabled={
-              termsToPractice(lessonTerms, leitnerBoxState, today) === 0
-            }
+            to={`/set/${set.id}/lessons`}
+            disabled={termsToPractice(set.terms, leitnerBoxState, today) === 0}
           >
-            New terms
+            Practice set
           </StyledLink>
-          <StyledLink
-            to={`/practice/lesson/${lessonName}/true`}
-            disabled={
-              termsToPractice(
-                getTerms(lessonName, true),
-                leitnerBoxState,
-                today
-              ) === 0
-            }
-          >
-            All material
-          </StyledLink>
-          <button onClick={() => setShowTerms(!showTerms)}>
-            {showTerms ? "Hide terms" : `Show terms (${termsWithStats.length})`}
-          </button>
-        </StyledLessonLinks>
+        </StyledSetLinks>
       </StyledLessonHeader>
-      {showTerms && <TermCardList cards={termsWithStats.map((t) => t.card)} />}
     </li>
   );
 }
