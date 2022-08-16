9720c1cc48960d4f511b5ad05be35811a375537e
Charlie McVicker <charlie@mcvickernet.com>
2022-08-16 17:33:45 -0400

    align th/td

--- src/components/VocabSetTable.tsx
+++ src/components/VocabSetTable.tsx
@@ -8,8 +8,12 @@ import { VisuallyHidden } from "./VisuallyHidden";
 const StyledTable = styled.table`
   width: 100%;
   border-collapse: collapse;
+  th {
+    text-align: left;
+  }
   td {
     border-bottom: 1px solid ${theme.colors.LIGHT_GRAY};
+    text-align: left;
   }
   tr:nth-child(2n + 1) {
     td {
