90621b9be1fe299761c2133adefca3e84aca2e66
Charlie McVicker <charlie@mcvickernet.com>
2022-08-18 13:36:31 -0400

    view lesson table; more table styles

--- src/components/StyledTable.tsx
+++ src/components/StyledTable.tsx
@@ -4,12 +4,21 @@ import { theme } from "../theme";
 export const StyledTable = styled.table`
   width: 100%;
   border-collapse: collapse;
-  th {
+  margin-bottom: 32px;
+  padding: 8px;
+  th,
+  td {
     text-align: left;
   }
+
+  th {
+    border-bottom: 1px solid ${theme.colors.MED_GRAY};
+    padding: 0 8px;
+  }
+
   td {
     border-bottom: 1px solid ${theme.colors.LIGHT_GRAY};
-    text-align: left;
+    padding: 8px;
   }
   tr:nth-child(2n + 1) {
     td {
--- src/views/lessons/ViewLesson.tsx
+++ src/views/lessons/ViewLesson.tsx
@@ -1,11 +1,12 @@
 import React, { ReactElement } from "react";
 import { Navigate, useParams } from "react-router-dom";
-import { cards, keyForCard } from "../../data/clean-cll-data";
+import { Card, cards, keyForCard } from "../../data/clean-cll-data";
 import { nameForLesson } from "../../state/reducers/lessons";
 import { ReviewResult } from "../../state/reducers/leitnerBoxes";
 import { useCardsForTerms } from "../../utils/useCardsForTerms";
 import { useLesson } from "../../state/useLesson";
 import { SectionHeading } from "../../components/SectionHeading";
+import { StyledTable } from "../../components/StyledTable";
 
 export function ViewLesson(): ReactElement {
   const { lessonId } = useParams();
@@ -14,9 +15,9 @@ export function ViewLesson(): ReactElement {
 }
 
 const reviewResultNames: Record<ReviewResult, string> = {
-  ALL_CORRECT: "All correct!",
-  SINGLE_MISTAKE: "Only one mistake.",
-  REPEAT_MISTAKE: "Multiple mistakes.",
+  ALL_CORRECT: "All correct",
+  SINGLE_MISTAKE: "Only one mistake",
+  REPEAT_MISTAKE: "Multiple mistakes",
 };
 
 export function _ViewLesson({ lessonId }: { lessonId: string }): ReactElement {
@@ -26,21 +27,47 @@ export function _ViewLesson({ lessonId }: { lessonId: string }): ReactElement {
     Object.keys(reviewedTerms),
     keyForCard
   );
+
+  const reviewedCardsByStatus: Record<ReviewResult, Card[]> = Object.entries(
+    reviewedTerms
+  ).reduce<Record<ReviewResult, Card[]>>(
+    (grouped, [term, reviewResult]) => ({
+      ...grouped,
+      [reviewResult]: [...grouped[reviewResult], reviewedCards[term]],
+    }),
+    {
+      [ReviewResult.ALL_CORRECT]: [],
+      [ReviewResult.SINGLE_MISTAKE]: [],
+      [ReviewResult.REPEAT_MISTAKE]: [],
+    }
+  );
   return (
     <div>
-      <p>
-        {nameForLesson(lesson)} - {new Date(lesson.createdFor).toDateString()}
-      </p>
-      <ul>
-        {Object.entries(reviewedTerms).map(([term, value], idx) => (
-          <li key={idx}>
-            <SectionHeading>
-              {reviewedCards[term].syllabary} / {reviewedCards[term].english}
-            </SectionHeading>
-            <p>{reviewResultNames[value]}</p>
-          </li>
-        ))}
-      </ul>
+      <SectionHeading>{nameForLesson(lesson)}</SectionHeading>
+      {Object.entries(reviewedCardsByStatus).map(
+        ([result, cards]) =>
+          cards.length > 0 && (
+            <div>
+              <h3>{reviewResultNames[result as ReviewResult]}</h3>
+              <StyledTable>
+                <thead>
+                  <tr>
+                    <th>Syllabary</th>
+                    <th>English translation</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {cards.map((card) => (
+                    <tr>
+                      <td>{card.syllabary}</td>
+                      <td>{card.english}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </StyledTable>
+            </div>
+          )
+      )}
     </div>
   );
 }
