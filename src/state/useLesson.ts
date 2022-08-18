2caf67e7c522b4dfb70236fa0776215ae914c5e6
Charlie McVicker <charlie@mcvickernet.com>
2022-08-18 10:37:48 -0400

    whitespace

--- src/state/useLesson.ts
+++ src/state/useLesson.ts
@@ -2,6 +2,7 @@ import { useReviewedTerms } from "../spaced-repetition/useReviewSession";
 import { Lesson } from "./reducers/lessons";
 import { ReviewResult } from "./reducers/leitnerBoxes";
 import { useUserStateContext } from "./UserStateProvider";
+import { LessonCreationError } from "./reducers/lessons/createNewLesson";
 
 interface UseLessonReturn {
   lesson: Lesson;
@@ -18,7 +19,9 @@ export function useLesson(lessonId: string): UseLessonReturn {
   const { lessons, concludeLesson, startLesson } = useUserStateContext();
   const reviewedTerms = useReviewedTerms(lessonId);
   const lesson = lessons[lessonId];
+
   if (!lesson) throw new Error(`Lesson ${lessonId} not found`);
+
   return {
     lesson,
     ...reviewedTerms,
