a091d7bea5e4e6939d7ea2fb70957c1e404c9827
Charlie McVicker <charlie@mcvickernet.com>
2022-08-29 22:53:26 -0400

    make sure outputs from migration are sanitized

--- src/data/migrations/index.ts
+++ src/data/migrations/index.ts
@@ -3,7 +3,8 @@ import { cherokeeToKey } from "../cards";
 export function applyMigration(
   term: string,
   migration: Record<string, string>
-) {
-  if (term.startsWith("wě:sasgo")) console.log(cherokeeToKey(term));
-  return migration[cherokeeToKey(term)] ?? term;
+): string {
+  const key = cherokeeToKey(term);
+  if (!(key in migration)) return key;
+  else return cherokeeToKey(migration[cherokeeToKey(term)]);
 }
