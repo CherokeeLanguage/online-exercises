88e61850c7f93d654e140978b651638bfd47f70c
Charles McVicker <5622360+CharlieMcVicker@users.noreply.github.com>
2022-09-27 14:03:30 -0400

    finish see say write! (#54)
    * finish see say write!
    
    * fix typos!

--- update-ssw-data.zsh
+++ update-ssw-data.zsh
@@ -1,2 +1,7 @@
-cp ../cherokee-audio-data/see-say-write/ssw-audio/* ./public/ssw-audio
-cp ../cherokee-audio-data/see-say-write/{ssw-cards,ssw}.json ./src/data/collections/
+cp ../cherokee-audio-data/see-say-write/ssw-audio/* ./public/ssw-audio # copy cherokee audio
+cp ../cherokee-audio-data/see-say-write/ssw.json ./src/data/collections/ # copy lessons
+# generate english audio and store a copy of the cards with english audio linked
+$(
+    cd ../audio-lessons-generator-python
+    ./online_exercises/generate-ssw-audio.zsh
+)
