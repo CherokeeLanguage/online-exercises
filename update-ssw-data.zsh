21c2cfd3ce10335c7529659fe4c5370bf7b03d80
Charlie McVicker <charlie@mcvickernet.com>
2022-10-19 12:00:45 -0400

    update paths to support more sets

--- update-ssw-data.zsh
+++ update-ssw-data.zsh
@@ -1,5 +1,5 @@
-cp ../cherokee-audio-data/see-say-write/ssw-audio/* ./public/ssw-audio # copy cherokee audio
-cp ../cherokee-audio-data/see-say-write/ssw.json ./src/data/collections/ # copy lessons
+cp ../cherokee-audio-data/see-say-write/online-exercises-audio/* ./public/online-exercises-audio # copy cherokee audio
+cp ../cherokee-audio-data/ssw.json ./src/data/collections/ # copy lessons
 # generate english audio and store a copy of the cards with english audio linked
 $(
     cd ../audio-lessons-generator-python
