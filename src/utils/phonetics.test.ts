b1095c2697cdbd7ecfd11e18b4a4aa8589e22a6b
Charlie McVicker <charlie@mcvickernet.com>
2022-12-02 13:51:35 -0500

    stub for a test

--- src/utils/phonetics.test.ts
+++ src/utils/phonetics.test.ts
@@ -1,13 +1,18 @@
 import assert from "assert";
-import { removeTonesAndMarkers } from "./phonetics";
+import {
+  normalizeAndRemovePunctuation,
+  removeTonesAndMarkers,
+} from "./phonetics";
 
 describe("removeTonesAndVowelLength", () => {
   it.each([
     ["sǔ:dáli", "sudali"],
-    ["sa:sa aná:ɂi", "sasa anai"],
-    ["U:ni:ji:ya dù:hyoha na asgaya", "Unijiya duhyoha na asgaya"],
+    ["Sa:sa aná:ɂi", "sasa anai"],
+    ["U:ni:ji:ya dù:hyoha na asgaya", "unijiya duhyoha na asgaya"],
   ])("works for a bunch of examples", (richPhonetics, expected) => {
-    const actual = removeTonesAndMarkers(richPhonetics);
+    const actual = removeTonesAndMarkers(
+      normalizeAndRemovePunctuation(richPhonetics)
+    );
     assert.deepStrictEqual(actual, expected, "Should produce expected output");
   });
 });
