9225898cde0951b75f7b127c6010a8d8e5c1f887
Charlie McVicker <charlie@mcvickernet.com>
2023-02-13 21:58:41 -0500

    more credit cleanup

--- src/components/CollectionCredits.tsx
+++ src/components/CollectionCredits.tsx
@@ -1,5 +1,23 @@
 import { ReactElement } from "react";
+import styled from "styled-components";
 import { Collection } from "../data/vocabSets";
+
+const StyledList = styled.ul`
+  margin: 0;
+  li {
+    margin: 0;
+    p {
+      margin: 0;
+    }
+  }
+`;
+
+const StyledCreditsSection = styled.div`
+  margin-bottom: 10px;
+  h4 {
+    margin: 0;
+  }
+`;
 
 export function CollectionCredits({
   collection: { credits },
@@ -9,26 +27,38 @@ export function CollectionCredits({
   return (
     <div>
       <p>{credits.description}</p>
-      <h4>Credits</h4>
-      <ul>
-        {credits.credits.map(({ role, name }) => (
-          <li>
-            <strong>{role}</strong>: {name}
-          </li>
-        ))}
-      </ul>
+      <StyledCreditsSection>
+        <h4>Credits</h4>
+        <StyledList>
+          {credits.credits.map(({ role, name }) => (
+            <li>
+              <strong>{role}</strong>: {name}
+            </li>
+          ))}
+        </StyledList>
+      </StyledCreditsSection>
       {credits.externalResources.length > 0 && (
-        <>
+        <StyledCreditsSection>
           <h4>External resources</h4>
-          <ul>
+          <StyledList>
             {credits.externalResources.map((resource, i) => (
               <li key={i}>
-                <a href={resource.href}>{resource.name}</a>
-                {resource.notes && <p>{resource.notes}</p>}
+                {resource.notes ? (
+                  <p>
+                    <a target="_blank" href={resource.href}>
+                      {resource.name}
+                    </a>
+                    : {resource.notes}
+                  </p>
+                ) : (
+                  <a target="_blank" href={resource.href}>
+                    {resource.name}
+                  </a>
+                )}
               </li>
             ))}
-          </ul>
-        </>
+          </StyledList>
+        </StyledCreditsSection>
       )}
     </div>
   );
