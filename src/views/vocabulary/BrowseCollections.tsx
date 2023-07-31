01e45af606ba577bd5d41b2958dfd1f3ec840b92
Charlie McVicker <charlie@mcvickernet.com>
2023-07-31 13:30:25 -0400

    add new header to all views

--- src/views/vocabulary/BrowseCollections.tsx
+++ src/views/vocabulary/BrowseCollections.tsx
@@ -4,6 +4,8 @@ import { SectionHeading } from "../../components/SectionHeading";
 import { Collection, collections } from "../../data/vocabSets";
 import { useAnalyticsPageName } from "../../firebase/hooks";
 import { useUserStateContext } from "../../providers/UserStateProvider";
+import { HanehldaView } from "../../components/HanehldaView";
+import { DefaultNav } from "../../components/HanehldaView/HanehldaNav";
 
 export function BrowseCollections(): ReactElement {
   useAnalyticsPageName("Find new vocabulary");
@@ -45,38 +47,40 @@ export function BrowseCollections(): ReactElement {
       };
     }, [upstreamCollectionId, sets]);
   return (
-    <div>
-      <SectionHeading>Find new vocabulary</SectionHeading>
-      <p>
-        Here you can find new collections of vocabulary. Click the title of a
-        collection to see the lessons and terms contained.
-      </p>
-
-      {upstreamCollection && (
-        <>
-          <SectionHeading>Learning now</SectionHeading>
-          <CollectionDetails collection={upstreamCollection} />
-        </>
-      )}
-      <SectionHeading>Unlearned collections</SectionHeading>
-      {availableCollections.length > 0 ? (
-        availableCollections.map((collection, idx) => (
-          <CollectionDetails collection={collection} key={idx} />
-        ))
-      ) : (
+    <HanehldaView navControls={<DefaultNav />} collapseNav>
+      <div>
+        <SectionHeading>Find new vocabulary</SectionHeading>
         <p>
-          There are no new available collections. Great job working through all
-          our content!
+          Here you can find new collections of vocabulary. Click the title of a
+          collection to see the lessons and terms contained.
         </p>
-      )}
-      {completedCollections.length > 0 && (
-        <>
-          <SectionHeading>Completed collections</SectionHeading>
-          {completedCollections.map((collection, idx) => (
+
+        {upstreamCollection && (
+          <>
+            <SectionHeading>Learning now</SectionHeading>
+            <CollectionDetails collection={upstreamCollection} />
+          </>
+        )}
+        <SectionHeading>Unlearned collections</SectionHeading>
+        {availableCollections.length > 0 ? (
+          availableCollections.map((collection, idx) => (
             <CollectionDetails collection={collection} key={idx} />
-          ))}
-        </>
-      )}
-    </div>
+          ))
+        ) : (
+          <p>
+            There are no new available collections. Great job working through
+            all our content!
+          </p>
+        )}
+        {completedCollections.length > 0 && (
+          <>
+            <SectionHeading>Completed collections</SectionHeading>
+            {completedCollections.map((collection, idx) => (
+              <CollectionDetails collection={collection} key={idx} />
+            ))}
+          </>
+        )}
+      </div>
+    </HanehldaView>
   );
 }
